c1cb973e510293c692a3f0e7e133006b9c759463
EnzoMbz <103067639+EnzoMbz@users.noreply.github.com>
2023-06-02 22:46:54 -0300

    Mudando Banco de dados

--- Atv_Pratica14_EnzoMb_DW1/resources-sql/Agenda_Atv_Pratica14.sql
+++ Atv_Pratica14_EnzoMb_DW1/resources-sql/Agenda_Atv_Pratica14.sql
@@ -9,16 +9,18 @@ CREATE TABLE `usuarios` (
   PRIMARY KEY (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-select nome, login from usuarios where login='eu' and senha='123';
-
 CREATE TABLE `tarefas` (
   `id_tarefa` int(11) NOT NULL,
-  `titulo` varchar(20) DEFAULT NULL,
+  `titulo` varchar(250) DEFAULT NULL,
   `descricao` varchar(250) DEFAULT NULL,
-  `data_criacao` varchar(20) DEFAULT NULL,
-  `data_conclusao` varchar(20) DEFAULT NULL,
+  `data_criacao` datetime DEFAULT current_timestamp,
+  `data_conclusao` datetime DEFAULT NULL,
   `status` boolean DEFAULT NULL,
   `id_usuario` int(11) NOT NULL,
   PRIMARY KEY (`id_tarefa`),
   CONSTRAINT fk_idUser FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+drop table tarefas;
+select * from tarefas;
+select * from usuarios;
